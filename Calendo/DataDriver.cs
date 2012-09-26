d23d52911a2b54ad74c332634ddf2007006d2d00
Nicholas <Nicholas@Nicholas-PC>
2012-09-26 21:24:09 +0800

    Added: Data storage component

---
+++ Calendo/DataDriver.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calendo
+{
+    class DataDriver
+    {
+        private DataStorage storage;
+        private List<Entry> entryList;
+        public DataDriver()
+        {
+            entryList = new List<Entry>();
+            storage = new DataStorage();
+        }
+        public void Clear()
+        {
+            storage.Load();
+            storage.Entries.Clear();
+            storage.Save();
+        }
+        public void Test()
+        {
+            // Load
+            storage.Load();
+            entryList = storage.Entries;
+
+            // Add
+            entryList.Add(new Entry());
+
+            // Modify
+            entryList[0].Description = "Test";
+
+            // Save
+            storage.Entries = entryList; //optional
+            storage.Save();
+
+            // Reload
+            storage.Load();
+            entryList = storage.Entries;
+            
+            // Remove
+            entryList.RemoveAt(0);
+
+            // Save
+            storage.Save();
+        }
+    }
+}
