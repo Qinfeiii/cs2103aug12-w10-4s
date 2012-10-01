a6a5a24197668e1780e2db8405c08af1148bbfcc
Jerome Cheng <jcheng31@gmail.com>
2012-10-01 15:42:50 -0700

    Remove unused usings.

--- Calendo/MainWindow.xaml.cs
+++ Calendo/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Calendo
 {
@@ -26,7 +17,7 @@ namespace Calendo
         {
             InitializeComponent();
             AutoSuggestViewModel = new AutoSuggest();
-            this.DataContext = AutoSuggestViewModel;
+            DataContext = AutoSuggestViewModel;
         }
 
         private void TbxCommandBarLostFocus(object sender, RoutedEventArgs e)
