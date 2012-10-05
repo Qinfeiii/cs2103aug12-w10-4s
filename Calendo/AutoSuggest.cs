3caabf44fb5818894c54a193256b05c1aef1454f
Nicholas <Nicholas@Nicholas-PC>
2012-10-05 22:50:00 +0800

    Merge with UI

--- Calendo/AutoSuggest.cs
+++ Calendo/AutoSuggest.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
 
 namespace Calendo
 {
@@ -11,18 +9,18 @@ namespace Calendo
     {
         public const char COMMAND_INDICATOR = '/';
 
-        public ObservableCollection<string> SuggestionList { get; set; }
+        public List<string> SuggestionList { get; set; }
 
         public AutoSuggest()
         {
-            SuggestionList = new ObservableCollection<string>();
+            SuggestionList = new List<string>();
         }
 
         public void SetSuggestions(string input)
         {
             SuggestionList.Clear();
             bool isInputValid = input.Length > 0;
-            if(isInputValid)
+            if (isInputValid)
             {
                 if (input.First() == COMMAND_INDICATOR)
                 {
@@ -36,7 +34,7 @@ namespace Calendo
                 else
                 {
                     SuggestionList.Add("search for " + input);
-                }   
+                }
             }
 
             OnPropertyChanged("SuggestionList");
---
+++ Calendo/GoogleCalendar/GoogleCalendar.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using DotNetOpenAuth.OAuth2;
+using Google.Apis.Authentication;
+using Google.Apis.Authentication.OAuth2;
+using Google.Apis.Authentication.OAuth2.DotNetOpenAuth;
+using Google.Apis.Tasks.v1;
+using Google.Apis.Tasks.v1.Data;
+using Google.Apis.Util;
+
+
+namespace Calendo.GoogleCalendar
+{
+    class GoogleCalendar
+    {
+
+        public void authorize()
+        {
+            Uri calendarUri = new Uri("https://www.google.com/calendar/feeds/rahij.test@gmail.com/private/full");
+            var provider = new NativeApplicationClient(GoogleAuthenticationServer.Description);
+            provider.ClientIdentifier = "253590999479.apps.googleusercontent.com";
+            provider.ClientSecret = "CVS5ZM83ji-d3a5fCMzMEBSP";
+            var auth = new OAuth2Authenticator<NativeApplicationClient>(provider, GetAuthentication);
+
+            // Create the service and register the previously created OAuth2 Authenticator.
+            var service = new TasksService(auth);
+        }
+
+        private static IAuthorizationState GetAuthentication(NativeApplicationClient arg)
+        {
+            // Get the auth URL:
+            IAuthorizationState state = new AuthorizationState(new[] { TasksService.Scopes.Tasks.GetStringValue() });
+            state.Callback = new Uri(NativeApplicationClient.OutOfBandCallbackUrl);
+            Uri authUri = arg.RequestUserAuthorization(state);
+
+            // Request authorization from the user (by opening a browser window):
+            Process.Start(authUri.ToString());
+            Console.Write("  Authorization Code: ");
+            string authCode = Console.ReadLine();
+            Console.WriteLine();
+
+            // Retrieve the access token by using the authorization code:
+            return arg.ProcessUserAuthorization(authCode, state);
+        }
+
+    }
+}
