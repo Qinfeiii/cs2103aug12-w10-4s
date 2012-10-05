742261254d8c323dd6b1ae434b2ce25277cba892
Nicholas <Nicholas@Nicholas-PC>
2012-10-05 23:07:55 +0800

    Changed: Documentation for several files

--- Calendo/JSON.cs
+++ Calendo/JSON.cs
@@ -6,13 +6,28 @@ using System.Web.Script.Serialization;
 
 namespace Calendo
 {
-    class JSON<T>
+    /// <summary>
+    /// Javascript Object Notation Parser
+    /// </summary>
+    /// <typeparam name="T">Type to convert to and from JSON</typeparam>
+    public class JSON<T>
     {
+        /// <summary>
+        /// Serialize an object to JSON
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
         public string Serialize(T obj) {
             var jss = new JavaScriptSerializer();
             var json = jss.Serialize(obj);
             return json.ToString();
         }
+
+        /// <summary>
+        /// Deserialize an object from JSON
+        /// </summary>
+        /// <param name="json"></param>
+        /// <returns></returns>
         public T Deserialize(string json)
         {
             var jss = new JavaScriptSerializer();
