baecc1d93ed61ff51a3117b9746fa5a9364e3e73
Jerome Cheng <jcheng31@gmail.com>
2012-10-13 15:54:36 +0800

    Add colouring of tasks based on status.

---
+++ Calendo/Converters/EntryToBrushConverter.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using Calendo.Data;
+
+namespace Calendo.Converters
+{
+    public class EntryToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Entry currentEntry = value as Entry;
+            if (currentEntry != null)
+            {
+                if (IsTaskOngoing(currentEntry))
+                {
+                    return Brushes.DarkOrange;
+                }
+                else if (IsTaskOverdue(currentEntry))
+                {
+                    return Brushes.DarkRed;
+                }
+            }
+
+            BrushConverter converter = new BrushConverter();
+            return converter.ConvertFrom("#FF464646") as Brush;
+        }
+
+        private bool IsTaskOverdue(Entry currentEntry)
+        {
+            bool isOverdue = false;
+
+            if (currentEntry.Type == EntryType.TIMED)
+            {
+                isOverdue = currentEntry.EndTime.CompareTo(DateTime.Now) < 0;
+            }
+            else if (currentEntry.Type == EntryType.DEADLINE)
+            {
+                isOverdue = currentEntry.StartTime.CompareTo(DateTime.Now) < 0;
+            }
+
+            return isOverdue;
+        }
+
+        private bool IsTaskOngoing(Entry currentEntry)
+        {
+            bool isOngoing = false;
+
+            TimeSpan nowAndTaskStartDifference = currentEntry.StartTime.Subtract(DateTime.Now);
+            bool isTaskStarting = 0 < nowAndTaskStartDifference.TotalHours && nowAndTaskStartDifference.TotalHours < 24;
+
+            if (currentEntry.Type == EntryType.TIMED)
+            {
+                bool isNowBetweenStartAndEnd = currentEntry.StartTime.CompareTo(DateTime.Now) < 0 &&
+                                               DateTime.Now.CompareTo(currentEntry.EndTime) < 0;
+                isOngoing = isTaskStarting || isNowBetweenStartAndEnd;
+            }
+            else if (currentEntry.Type == EntryType.DEADLINE)
+            {
+                isOngoing = isTaskStarting;
+            }
+
+            // Floating tasks can't be ongoing.
+            return isOngoing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
