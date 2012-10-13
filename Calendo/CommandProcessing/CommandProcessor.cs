7446bb3e803c0939b8b471e0f5dd86e60469c513
Nicholas <Nicholas@Nicholas-PC>
2012-10-13 19:25:57 +0800

    Fixed: Specifying invalid or no ID for change/remove no longer crashes application.

--- Calendo/CommandProcessing/CommandProcessor.cs
+++ Calendo/CommandProcessing/CommandProcessor.cs
@@ -85,14 +85,42 @@ namespace Calendo.CommandProcessing
 
         private void ExecuteRemove()
         {
-            int index = Convert.ToInt32(commandText) - 1;
+            if (commandText == null)
+            {
+                // Command without parameter
+                return;
+            }
+            int index = 0;
+            try
+            {
+                index = Convert.ToInt32(commandText) - 1;
+            }
+            catch
+            {
+                // Invalid ID
+                return;
+            }
             taskManager.RemoveByIndex(index);
         }
 
         private void ExecuteChange()
         {
+            if (commandText == null)
+            {
+                // Command without parameter
+                return;
+            }
             string[] commandTextPieces = commandText.Split();
-            int taskNumberToChange = Convert.ToInt32(commandTextPieces.First());
+            int taskNumberToChange = 0;
+            try
+            {
+                taskNumberToChange = Convert.ToInt32(commandTextPieces.First());
+            }
+            catch
+            {
+                // Invalid id
+                return;
+            }
             List<string> listOfCommandTextPieces = commandTextPieces.ToList();
             listOfCommandTextPieces.RemoveAt(0);
             string newTaskName = "";
