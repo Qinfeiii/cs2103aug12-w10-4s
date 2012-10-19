b5132d8050b036d3a9e33575278d439241e85fd1
Jerome Cheng <jcheng31@gmail.com>
2012-10-19 19:16:33 +0800

    Add coloured images for window buttons.

---
+++ Calendo/AutoSuggest/AutoSuggest.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Calendo.AutoSuggest
+{
+    class AutoSuggest : INotifyPropertyChanged
+    {
+        public const char COMMAND_INDICATOR = '/';
+
+        public List<AutoSuggestEntry> SuggestionList { get; set; }
+
+        private List<AutoSuggestEntry> MasterList; 
+
+        public AutoSuggest()
+        {
+            MasterList = new List<AutoSuggestEntry>
+                                 {
+                                     new AutoSuggestEntry("/add", "add a new item", EntryType.MASTER),
+                                     new AutoSuggestEntry("/change", "edit an item", EntryType.MASTER),
+                                     new AutoSuggestEntry("/remove", "remove an item", EntryType.MASTER),
+                                     new AutoSuggestEntry("/undo", "undo the last action", EntryType.MASTER),
+                                     new AutoSuggestEntry("/redo", "revert an undone action", EntryType.MASTER),
+                                     new AutoSuggestEntry("/sync", "synchronize with Google Calendar", EntryType.MASTER),
+                                     
+                                     new AutoSuggestEntry("/add", "[description] /date [DD/MM] /time [HH:MM]", EntryType.DETAIL),
+                                     new AutoSuggestEntry("/change", "[number] [description]", EntryType.DETAIL),
+                                     new AutoSuggestEntry("/remove", "[number]", EntryType.DETAIL)
+                                 };
+            SuggestionList = new List<AutoSuggestEntry>();
+        }
+
+        public void SetSuggestions(string input)
+        {
+            SuggestionList.Clear();
+            bool isInputValid = input.Length > 0;
+
+            string[] inputWords = input.Split();
+            string inputCommand = inputWords[0];
+
+            if (isInputValid && input.First() == COMMAND_INDICATOR)
+            {
+                if(inputWords.Length == 1)
+                {
+                    // Only a command has been entered.
+                    SuggestionList = new List<AutoSuggestEntry>(MasterList.Where(o => o.Type == EntryType.MASTER && o.Command.Contains(inputCommand)));
+                }
+                else if (inputWords.Length > 1)
+                {
+                    // Command has been entered. Show parameter suggestions.
+                    SuggestionList = new List<AutoSuggestEntry>(MasterList.Where(o => o.Type == EntryType.DETAIL && o.Command.Contains(inputCommand)));
+                }
+            }
+
+            OnPropertyChanged("SuggestionList");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventArgs args = new PropertyChangedEventArgs(propertyName);
+            PropertyChangedEventHandler changed = PropertyChanged;
+            if (changed != null)
+            {
+                changed(this, args);
+            }
+        }
+    }
+}
---
+++ Calendo/Debug/DebugMode.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Calendo.Data;
+using Calendo.CommandProcessing;
+using Calendo.GoogleCalendar;
+
+namespace Calendo
+{
+    /// <summary>
+    /// Interaction logic for DebugMode.xaml
+    /// </summary>
+    public partial class DebugMode : Window
+    {
+        // NOTE: This is a testing class with a GUI interface
+        // Used for exploratory testing for TaskManager, SettingsManager, CommandProcessor
+        TaskManager tm = new TaskManager();
+        CommandProcessor cp = new CommandProcessor();
+        public DebugMode()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void UpdateList()
+        {
+            this.listBox1.Items.Clear();
+            for (int i = 0; i < tm.Entries.Count; i++)
+            {
+                this.listBox1.Items.Add("[" + tm.Entries[i].ID.ToString() + "] " + tm.Entries[i].Description);
+            }
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            // Used for testing CP
+            if (e.Key == Key.Enter)
+            {
+                cp.ExecuteCommand(textBox1.Text);
+                tm.Load(); // Update TM with changes done by CP
+                UpdateList();
+                textBox1.Text = "";
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            // Bypass CP (warning: This is not sync with CP - so CP will use outdated list)
+            tm.Add(this.textBox1.Text);
+            this.textBox1.Text = "";
+            UpdateList();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            // Bypass CP (warning: This is not sync with CP - so CP will use outdated list)
+            if (this.listBox1.Items.Count > 0 && this.listBox1.SelectedIndex >= 0)
+            {
+                tm.Remove(tm.Entries[this.listBox1.SelectedIndex].ID);
+            }
+            UpdateList();
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            // Bypass CP (warning: This is not sync with CP - so CP will use outdated list)
+            tm.Undo();
+            UpdateList();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateList();
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("This button is not in use");
+            MessageBox.Show(GoogleCalendar.GoogleCalendar.Import());
+        }
+
+        private void button5_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsManager sm = new SettingsManager();
+            sm.GetSetting("null");
+            sm.SetSetting("test", "a");
+            MessageBox.Show(sm.GetSetting("test"));
+        }
+    }
+}
