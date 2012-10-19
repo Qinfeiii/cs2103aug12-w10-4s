92f89f6f35a74d807e654a8f45d978ec93db9390
Rahij Ramsharan <rahij.ramsharan@gmail.com>
2012-10-19 14:31:08 +0800

    general task fetch implemented in gcal

--- Calendo/GoogleCalendar/GoogleCalendar.cs
+++ Calendo/GoogleCalendar/GoogleCalendar.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Collections.Generic;
+using System.Web;
 using System.Text;
 using System.IO;
 using System.Diagnostics;
@@ -11,22 +12,29 @@ using Google.Apis.Authentication.OAuth2.DotNetOpenAuth;
 using Google.Apis.Tasks.v1;
 using Google.Apis.Tasks.v1.Data;
 using Google.Apis.Util;
+using Newtonsoft.Json;
 
 
 namespace Calendo.GoogleCalendar
 {
     class GoogleCalendar
     {
-	
-	public static bool Sync(List<Entry> entries){
-	   String auth = Authorize();
-        //postTasks(tasks, auth);
-       return false;
-	}
-	public static String Import(){
-			String auth = Authorize();
+
+        public static bool Sync(List<Entry> entries)
+        {
+            string auth = Authorize();
+            //postTasks(tasks, auth);
+            return false;
+        }
+        public static string Import()
+        {
+            string auth = Authorize();
+            if (auth == "")
+            {
+                return "";
+            }
             string sURL;
-            sURL = " https://www.googleapis.com/tasks/v1/lists/MTU4OTEwNzMxMTYxNzgzMjEwNDc6MDow/tasks?access_token=" + auth;
+            sURL = "https://www.googleapis.com/tasks/v1/lists/" + getTaskListId(auth) + "/tasks?access_token=" + auth;
 
             WebRequest wrGETURL;
             wrGETURL = WebRequest.Create(sURL);
@@ -39,52 +47,85 @@ namespace Calendo.GoogleCalendar
             string sLine = "";
             int i = 0;
 
-            String tasks="";
+            string tasks = "";
             while (sLine != null)
             {
                 i++;
                 sLine = objReader.ReadLine();
                 if (sLine != null)
-                    tasks+=i + ": " + sLine;
+                    tasks += i + ": " + sLine;
             }
-        return tasks;
-	}
-	private static string Authorize()
-        { 
+            return tasks;
+        }
+
+        private static string getTaskListId(String auth)
+        {
+            string sURL;
+            sURL = " https://www.googleapis.com/tasks/v1/users/@me/lists?access_token=" + auth;
+            WebRequest wrGETURL;
+            wrGETURL = WebRequest.Create(sURL);
+
+            Stream objStream;
+            objStream = wrGETURL.GetResponse().GetResponseStream();
+
+            StreamReader objReader = new StreamReader(objStream);
+            string sLine = "", taskListDetails = "";
+            int i = 0;
+
+
+            while (sLine != null)
+            {
+                i++;
+                sLine = objReader.ReadLine();
+                if (sLine != null)
+                    taskListDetails += sLine;
+            }
+
+            JSON<TI> jtest = new JSON<TI>();
+            TI values = jtest.Deserialize(taskListDetails);
+            String taskListId = "";
+            for (int c = 0; c < values.items.Count; c++)
+            {
+                taskListId += Console.WriteLine(values.items[c].id);
+            }
+            return taskListId;
+        }
+        private static string Authorize()
+        {
             var provider = new NativeApplicationClient(GoogleAuthenticationServer.Description);
             provider.ClientIdentifier = "770362652845-cb7ki86iesscd3f54vs8nd063epao8v3.apps.googleusercontent.com";
-            String auth = GetAuthentication(provider);
+            string auth = GetAuthentication(provider);
             return auth;
         }
 
-        private static String GetAuthentication(NativeApplicationClient provider)
+        private static string GetAuthentication(NativeApplicationClient provider)
         {
-            String url="https://accounts.google.com/o/oauth2/auth?";
+            string url = "https://accounts.google.com/o/oauth2/auth?";
             url += "scope=https://www.googleapis.com/auth/userinfo.email+https://www.googleapis.com/auth/userinfo.profile+https://www.googleapis.com/auth/tasks";
-            url+="&redirect_uri=http://rahij.com/calendo.php";
-            url+="&response_type=token";
+            url += "&redirect_uri=http://rahij.com/calendo.php";
+            url += "&response_type=token";
             url += "&client_id=" + provider.ClientIdentifier;
-            Console.WriteLine(url);
             Uri authUri = new Uri(url);
-            
+
             // Request authorization from the user (by opening a browser window):
             Process.Start(authUri.ToString());
-            Console.Write("  Authorization Code: ");
-            string authCode = Console.ReadLine();
-            Console.WriteLine();
+
+            AskAuth a = new AskAuth();
+            a.ShowDialog();
+            string authCode = a.authCode;
 
             // Retrieve the access token by using the authorization code:
             return authCode;
         }
-		
-        private static void postTasks(Entry tasks, String auth)
+
+        private static void postTasks(Entry tasks, string auth)
         {
             HttpWebRequest httpWReq =
-            (HttpWebRequest)WebRequest.Create("https://www.googleapis.com/tasks/v1/lists/MTU4OTEwNzMxMTYxNzgzMjEwNDc6MDow/tasks?access_token="+auth);
+            (HttpWebRequest)WebRequest.Create("https://www.googleapis.com/tasks/v1/lists/MTU4OTEwNzMxMTYxNzgzMjEwNDc6MDow/tasks?access_token=" + auth);
 
             ASCIIEncoding encoding = new ASCIIEncoding();
             //string postData = "{ kind: tasks#task,";
-            string postData = "\"title\": lol_its_task"; 
+            string postData = "\"title\": lol_its_task";
             //postData += "status: completed}";
 
             byte[] data = encoding.GetBytes(postData);
@@ -95,7 +136,7 @@ namespace Calendo.GoogleCalendar
 
             using (Stream newStream = httpWReq.GetRequestStream())
             {
-                newStream.Write(data,0,data.Length);
+                newStream.Write(data, 0, data.Length);
             }
         }
 
