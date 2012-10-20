12f71d3bfc732792dcc533e50a079cc72a26600c
Rahij Ramsharan <rahij.ramsharan@gmail.com>
2012-10-20 22:24:42 +0800

    task posting (only titles) added

---
+++ Calendo/GoogleCalendar/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GCalTest
+{
+    public class TI
+    {
+        public string kind
+        {
+            get;
+            set;
+        }
+        public string etag
+        {
+            get;
+            set;
+        }
+        public List<TE> items
+        {
+            get;
+            set;
+        }
+
+    }
+    public class TE
+    {
+        public string kind
+        {
+            get;
+            set;
+        }
+        public string id
+        {
+            get;
+            set;
+        }
+        public string title
+        {
+            get;
+            set;
+        }
+        public string updated
+        {
+            get;
+            set;
+        }
+        public string selfLink
+        {
+            get;
+            set;
+        }
+    }
+}
