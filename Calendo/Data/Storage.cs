61b41f373c8b61f187caf0687a81b204d5bc22c9
Nicholas <Nicholas@Nicholas-PC>
2012-10-21 22:57:59 +0800

    Added: Complete status for entries Added: Documentation for some methods

--- Calendo/Data/Storage.cs
+++ Calendo/Data/Storage.cs
@@ -8,14 +8,24 @@ using Calendo.Diagnostics;
 
 namespace Calendo.Data
 {
+    /// <summary>
+    /// Wrapper class for object used in serialization
+    /// </summary>
+    /// <typeparam name="T">Generic type to wrap</typeparam>
     [Serializable]
     [XmlRoot("Data")]
     public class Data<T> where T : new()
     {
+        /// <summary>
+        /// Creates a new Data object
+        /// </summary>
         public Data() {
             Value = new T();
         }
 
+        /// <summary>
+        /// Gets or sets the value represented by the object
+        /// </summary>
         [XmlElement("Entry")]
         public T Value
         {
