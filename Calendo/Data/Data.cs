efa37bf4215d83023bd1832fabb54dd4f01e3f0b
Nicholas <Nicholas@Nicholas-PC>
2012-10-30 21:54:12 +0800

    Changed: Entry class moved to Logic component Changed: JSON class moved to GoogleCalendar component Added: Tagged author to code Changed: Adding dates in past re-enabled Changed: Ability to remove date and time by specfiying hyphen Changed: Data and State classes moved to separate files Changed: Assertion added for StateStorage class Changed: Changes to how State accesses list of entries Fixed: Changing only date will not affect time and vice versa Added: DebugTool contains several properties to access notification information

---
+++ Calendo/Data/Data.cs
@@ -0,0 +1,35 @@
+﻿//@author Nicholas
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Calendo.Data
+{
+    /// <summary>
+    /// Wrapper class for object used in serialization
+    /// </summary>
+    /// <typeparam name="T">Generic type to wrap</typeparam>
+    [Serializable]
+    [XmlRoot("Data")]
+    public class Data<T> where T : new()
+    {
+        /// <summary>
+        /// Creates a new Data object
+        /// </summary>
+        public Data()
+        {
+            Value = new T();
+        }
+
+        /// <summary>
+        /// Gets or sets the value represented by the object
+        /// </summary>
+        [XmlElement("Entry")]
+        public virtual T Value
+        {
+            get;
+            set;
+        }
+    }
+}
--- Calendo/Data/StateStorage.cs
+++ Calendo/Data/StateStorage.cs
@@ -1,139 +1,12 @@
-﻿using System;
+﻿//@author Nicholas
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Xml.Serialization;
-using System.IO;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
 using System.Diagnostics;
 
 namespace Calendo.Data
 {
-    [Serializable]
-    [XmlRoot("Data")]
-    public class State<T> where T : new()
-    {
-        private static Stack<T> RedoStack = new Stack<T>();
-
-        /// <summary>
-        /// Create a State object to represent object states
-        /// </summary>
-        public State()
-        {
-            this.Value = new T();
-            this.States = new List<T>();
-        }
-
-        /// <summary>
-        /// Current State
-        /// </summary>
-        public T Value
-        {
-            get;
-            set;
-        }
-
-        /// <summary>
-        /// List of States
-        /// </summary>
-        [XmlArrayItem("State")]
-        public List<T> States
-        {
-            get;
-            set;
-        }
-
-        /// <summary>
-        /// Adds a state
-        /// </summary>
-        public void AddState()
-        {
-            if (this.States.Count == 0)
-            {
-                this.States.Add(new T());
-            }
-            this.States.Add(PerformClone(Value));
-            RedoStack.Clear();
-        }
-
-        /// <summary>
-        /// Revert to state before last state
-        /// </summary>
-        /// <returns>Return true if state is changed</returns>
-        public bool Undo()
-        {
-            if (this.States.Count > 1)
-            {
-                // First state does not count
-                RedoStack.Push(this.States[States.Count - 1]);
-                this.States.RemoveAt(this.States.Count - 1);
-                this.Value = PerformClone(this.States[States.Count - 1]);
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        /// <summary>
-        /// Gets a boolean value determining if there are undo states
-        /// </summary>
-        public bool HasUndo
-        {
-            get
-            {
-                // First state does not count
-                return (this.States.Count > 1);
-            }
-        }
-
-        /// <summary>
-        /// Revert to state in redo stack
-        /// </summary>
-        /// <returns>Return true if state is changed</returns>
-        public bool Redo()
-        {
-            if (RedoStack.Count > 0)
-            {
-                this.Value = PerformClone(RedoStack.Pop());
-                this.States.Add(Value);
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        /// <summary>
-        /// Gets a boolean value determining if there are redo states
-        /// </summary>
-        public bool HasRedo
-        {
-            get
-            {
-                return (RedoStack.Count > 0);
-            }
-        }
-
-        /// <summary>
-        /// Clones the object
-        /// </summary>
-        /// <param name="obj">Provided object</param>
-        /// <returns>Deep copy of the object</returns>
-        private T PerformClone(T obj)
-        {
-            Debug.Assert(typeof(T).IsSerializable, "Object is not serializable!");
-            MemoryStream memoryStream = new MemoryStream();
-            BinaryFormatter formatter = new BinaryFormatter(null, new StreamingContext(StreamingContextStates.Clone));
-            formatter.Serialize(memoryStream, obj);
-            memoryStream.Seek(0, SeekOrigin.Begin);
-            T clone = (T)formatter.Deserialize(memoryStream);
-            return clone;
-        }
-    }
-
     public class StateStorage<T> : Storage<T> where T : new()
     {
         private Storage<State<T>> dataStorage;
@@ -249,6 +122,7 @@ namespace Calendo.Data
             this.dataStorage.Entries.States.Clear();
             this.dataStorage.Save();
             this.dataStorage.Load();
+            Debug.Assert(this.dataStorage.Entries != null);
         }
     }
 }
