1447bfd67e4fbf7a441cb377ed81efa3d238c3a9
pshinghal <pshinghal@gmail.com>
2012-11-07 17:44:57 +0800

    Added Command class

---
+++ Calendo/Logic/Command.cs
@@ -0,0 +1,29 @@
+﻿//@author A0091571E
+
+namespace Calendo.Logic
+{
+    class Command
+    {
+        public string StartDate { get; set; }
+        public string StartTime { get; set; }
+        public string EndDate { get; set; }
+        public string EndTime { get; set; }
+        public string Type { get; set; }
+        public string Text { get; set; }
+
+        public Command()
+        {
+            Initialise();
+        }
+
+        private void Initialise()
+        {
+            StartDate = null;
+            StartTime = null;
+            EndDate = null;
+            EndTime = null;
+            Type = null;
+            Text = null;
+        }
+    }
+}
